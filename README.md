e002aec8e64c8992aafc9a23bda91b2b673f91f7
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-17 10:27:04 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+LifterLMS Customizations
+------------------------
+
+Use this plugin as a [site-specific plugin](http://www.wpbeginner.com/beginners-guide/what-why-and-how-tos-of-creating-a-site-specific-wordpress-plugin/).
+Add any custom actions, filters, or functions to your WordPress site.
+When updating LifterLMS, LifterLMS LaunchPad or other plugins your customizations will not be lost if you store them in this plugin.
+
+Add any custom code to "lifterlms-customizations.php"
---
+++ lifterlms-customizations.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Plugin Name: LifterLMS Customizations
+* Plugin URI: https://lifterlms.com/
+* Description: Add custom functions to LifterLMS or LifterLMS LaunchPad and preserve them when updating!
+* Version: 1.0.0
+* Author: codeBOX
+* Author URI: http://gocodebox.com
+* Text Domain: lifterlms-customizations
+* Domain Path: /languages
+* License:     GPLv2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Requires at least: 4.0
+* Tested up to: 4.6
+*
+* @package 		LifterLMS
+* @category 	Core
+* @author 		codeBOX
+*/
+
+/**
+ * Restrict direct access
+ */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/**
+ * Add custom functions below this line
+ */
